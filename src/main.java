213286536dcdcbbc6d6012ccb12071f9f7cb34d8
abualrubbaraa <baraaabualrub1998@gmail.com>
2020-06-11 00:05:00 +0300

    fixing pulling problem

--- src/main.java
+++ src/main.java
@@ -1,13 +1,3 @@
-//import java.util.Timer;
-//import java.util.TimerTask;
-//class Helper extends TimerTask
-//{
-//    public static int i = 0;
-//    public void run()
-//    {
-//        System.out.println("Timer ran " + ++i);
-//    }
-//}
 
 import Backend.GameTools.Player;
 import Backend.MapFactory;
